614a9cdfca670a2678eda4fbdf025d1a361b7cc2
ggaiti <ggaiti@hotmail.com>
2023-05-13 17:45:09 -0400

    server

--- server/index.js
+++ server/index.js
@@ -20,5 +20,3 @@ mongoose
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
   .catch((error) => console.log(error.message));
-
-//asd
--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -19,3 +19,5 @@ const postSchema = mongoose.Schema({
 const PostMessage = mongoose.model("PostMessage", postSchema);
 
 export default PostMessage;
+
+console.log("hi");
