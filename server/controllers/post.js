d28795768be57ab1f91473e18a799fa0c3626754
Alexander Rodriguez <Code.Alex.Rodriguez@outlook.com>
2023-05-13 21:00:35 -0400

    client push

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -5,6 +5,7 @@ export const getPosts = async (req, res) => {
     const postMessages = await PostMessage.find();
     res.status(200).json(postMessages);
   } catch (error) {
+
     res.status(404).json({ message: error.message });
   }
 };
