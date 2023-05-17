158c68569686b8f172cbddb08c2bdd527ed9f167
ggaiti <ggaiti@hotmail.com>
2023-05-17 16:31:26 -0400

    added env file

--- server/index.js
+++ server/index.js
@@ -3,19 +3,24 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/post.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
-const CONNECTION_URL =
-  "mongodb+srv://ggaiti:gaiti123@cluster0.r0jyohs.mongodb.net/?retryWrites=true&w=majority";
+// const CONNECTION_URL =
+//   "mongodb+srv://ggaiti:gaiti123@cluster0.r0jyohs.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
